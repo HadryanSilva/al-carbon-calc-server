db7597b54a601fd9ee257a2ba5dd34f22da8c9eb
Hadryan Silva <hadryan2001@gmail.com>
2025-01-03 15:33:19 -0300

    Add validation for recyclePercentage

--- src/main/java/br/com/actionlabs/carboncalc/dto/UpdateCalcInfoRequestDTO.java
+++ src/main/java/br/com/actionlabs/carboncalc/dto/UpdateCalcInfoRequestDTO.java
@@ -1,5 +1,7 @@
 package br.com.actionlabs.carboncalc.dto;
 
+import jakarta.validation.constraints.DecimalMax;
+import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.Data;
@@ -18,6 +20,8 @@ public class UpdateCalcInfoRequestDTO {
   @NotBlank
   private int solidWasteTotal;
   @NotBlank
+  @DecimalMin("0.0")
+  @DecimalMax("1.0")
   private double recyclePercentage;
 
 }
