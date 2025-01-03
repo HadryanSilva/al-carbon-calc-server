37913dbe0f7a9e9bc5819b3a5be1cbf7421ea404
Hadryan Silva <hadryan2001@gmail.com>
2025-01-03 11:27:11 -0300

    Fix solid waste calculation

--- src/main/java/br/com/actionlabs/carboncalc/service/SolidWasteCalculatorService.java
+++ src/main/java/br/com/actionlabs/carboncalc/service/SolidWasteCalculatorService.java
@@ -17,21 +17,22 @@ public class SolidWasteCalculatorService {
 
     public Integer calculateSolidWasteEmission(String uf, UpdateCalcInfoRequestDTO dto) {
         log.info("Calculating solid waste emission for UF: {}", uf);
-        var factor = solidWasteEmissionFactorRepository.findById(uf)
+        var factor = solidWasteEmissionFactorRepository.findByUf(uf)
                 .orElseThrow(() -> new ResourceNotFoundException("UF not found"));
-        return calculateRecyclableWasteEmission(factor, dto) + calculateNonRecyclableWasteEmission(factor, dto);
+        return (int) (calculateRecyclableWasteEmission(factor, dto) +
+                calculateNonRecyclableWasteEmission(factor, dto));
     }
 
-    private int calculateRecyclableWasteEmission(SolidWasteEmissionFactor factors, UpdateCalcInfoRequestDTO dto) {
+    private Double calculateRecyclableWasteEmission(SolidWasteEmissionFactor factors, UpdateCalcInfoRequestDTO dto) {
         log.info("Calculating recyclable waste emission");
         var recyclableSolidWaste = dto.getSolidWasteTotal() * dto.getRecyclePercentage();
-        return (int) (recyclableSolidWaste * factors.getRecyclableFactor());
+        return (recyclableSolidWaste * factors.getRecyclableFactor());
     }
 
-    private int calculateNonRecyclableWasteEmission(SolidWasteEmissionFactor factors, UpdateCalcInfoRequestDTO dto) {
+    private Double calculateNonRecyclableWasteEmission(SolidWasteEmissionFactor factors, UpdateCalcInfoRequestDTO dto) {
         log.info("Calculating non-recyclable waste emission");
         var nonRecyclableSolidWaste = dto.getSolidWasteTotal() * (1 - dto.getRecyclePercentage());
-        return (int) (nonRecyclableSolidWaste * factors.getNonRecyclableFactor());
+        return (nonRecyclableSolidWaste * factors.getNonRecyclableFactor());
     }
 
 }
